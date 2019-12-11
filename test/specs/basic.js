a0ca665efd1943d1c1a014d3fe16ff5664cf416f
Andrii Derevianko <sof2025@gmail.com>
2019-12-11 12:03:20 -0700

    first commit

---
+++ test/specs/basic.js
@@ -0,0 +1,12 @@
+var title;
+describe('demo.asteriag.com landing page title validation', () => {
+it('Navigate to URL', () => {
+browser.url('http://demo.asteriag.com');
+});
+it('Get title', () => {
+title = browser.getTitle();
+});
+it('Title validation', () => {
+expect(title).toBe('Asteria Bank – We Test For Better Software');
+});
+})
